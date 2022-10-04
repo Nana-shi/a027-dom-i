4eae724e3248c77114b86586cc9b86ae94027bca
Nana-shi <leof.pereira2@gmail.com>
2022-10-03 22:46:42 -0300

    Atividades da aula a027-dom-1 03/10

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const frutas = ["laranja", "limão", "uva"];
+// const frutas = ["laranja", "limão", "uva"];
+
+function frutaNova(){
+    let textoDoInput = document.getElementById("fruta")
+    console.log(textoDoInput.value)
+}
+
+let fruta4 = document.getElementById("fruta-4")
+
+function adicionarFruta(){
+    fruta4.innerHTML = document.getElementById("fruta").value
+}
